c352f8862af0211754ab2480711817eac5aeb945
andreleao1 <andre.gustavo.ls@hotmail.com>
2024-09-14 11:57:26 -0300

    feature: refactoring the code to send message topic as json

--- src/main/java/com/agls/trading_service/infra/kafka/impl/KafkaProducerGatewayImpl.java
+++ src/main/java/com/agls/trading_service/infra/kafka/impl/KafkaProducerGatewayImpl.java
@@ -2,6 +2,7 @@ package com.agls.trading_service.infra.kafka.impl;
 
 import com.agls.trading_service.domain.models.BitcoinTradeModel;
 import com.agls.trading_service.infra.kafka.KafkaProducerGateway;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,12 +17,19 @@ public class KafkaProducerGatewayImpl implements KafkaProducerGateway {
     @Value("${spring.kafka.topics.order-trading-topic}")
     private String topicName;
 
-    private final KafkaTemplate<String, BitcoinTradeModel> kafkaTemplate;
+    private final KafkaTemplate<Object, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void sendToKafka(BitcoinTradeModel bitcoinTradeModel) {
-        log.info("Sending message to Kafka topic: {}, trade id: {}", topicName, bitcoinTradeModel.getTradeId());
-        kafkaTemplate.send(topicName, bitcoinTradeModel);
-        log.info("Message sent successfully.");
+        try {
+            log.info("Sending message to Kafka topic: {}, trade id: {}", topicName, bitcoinTradeModel.getTradeId());
+            var t = objectMapper.writeValueAsString(bitcoinTradeModel);
+            kafkaTemplate.send(topicName, t);
+            log.info("Message sent successfully.");
+        } catch (Exception e) {
+            log.error("Error sending message to Kafka topic: " + topicName, e);
+            throw new RuntimeException("Error sending message to Kafka topic: " + topicName, e);
+        }
     }
 }
